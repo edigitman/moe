024f5af3aef7511ccb9103eb0abe166acbbeb326
edi <edi.gitman@gmail.com>
2016-08-05 22:12:11 +0200

    Added stud exam answer service show resume after exam to student

--- src/main/java/ro/agitman/moe/dao/impl/StudExamAnswerDaoImpl.java
+++ src/main/java/ro/agitman/moe/dao/impl/StudExamAnswerDaoImpl.java
@@ -1,9 +1,19 @@
 package ro.agitman.moe.dao.impl;
 
+import org.mentabean.BeanException;
 import org.mentabean.BeanSession;
+import org.mentabean.util.SQLUtils;
 import ro.agitman.moe.dao.StudExamAnswerDao;
+import ro.agitman.moe.model.Exam;
+import ro.agitman.moe.model.ExamItem;
 import ro.agitman.moe.model.StudentExamAnswer;
+import ro.agitman.moe.web.dto.StudentItemAnswerDTO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,6 +43,45 @@ public class StudExamAnswerDaoImpl extends GenericDaoImpl<StudentExamAnswer> imp
         answer.setOwnerId(studId);
 
         return beanSession.loadList(answer);
+    }
+
+    public List<StudentItemAnswerDTO> findWithItemByStudentInstance(Integer studExiId){
+        List<StudentItemAnswerDTO> result = new ArrayList<>();
+
+        Connection conn = beanSession.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rset = null;
+
+        try {
+            StringBuilder query = new StringBuilder(256);
+            query.append("select ");
+            query.append(beanSession.buildSelect(StudentExamAnswer.class, "answer"));
+            query.append(", ");
+            query.append(beanSession.buildSelect(ExamItem.class, "item"));
+            query.append(" from student_exam_answers answer, exam_items item ");
+            query.append(" where answer.examitemid = item.id and answer.studentexaminstanceid = ? ");
+
+            stmt = SQLUtils.prepare(conn, query.toString(), studExiId);
+            rset = stmt.executeQuery();
+
+            while(rset.next()){
+
+                StudentExamAnswer answer = new StudentExamAnswer();
+                beanSession.populateBean(rset, answer, "answer");
+
+                ExamItem item = new ExamItem();
+                beanSession.populateBean(rset, item, "item");
+
+                result.add(new StudentItemAnswerDTO(answer, item));
+            }
+
+            return result;
+
+        } catch (SQLException e) {
+            throw new BeanException(e);
+        } finally {
+            SQLUtils.close(rset, stmt);
+        }
     }
 
     public StudentExamAnswer findByExiStudItem(Integer exiId, Integer studId, Integer itemId) {
